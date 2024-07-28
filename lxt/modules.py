6ffd270e6da3ff85d2f368ee36e213cadf43dba2
David Bielik (tars) <git@dvdblk.com>
2024-07-28 20:44:42 +0200

    feat: add lora

--- lxt/modules.py
+++ lxt/modules.py
@@ -10,6 +10,7 @@ import torch.fx
 ### LRP Modules ###
 ###################
 
+
 class SoftmaxDT(nn.Softmax):
 
     def __init__(self, dim: int, dtype=None, temperature=1.0, inplace=False, **kwargs):
@@ -24,13 +25,22 @@ class SoftmaxDT(nn.Softmax):
 
 class LinearEpsilon(nn.Linear):
 
-    def __init__(self, in_features: int, out_features: int, bias: bool = True, device=None, dtype=None, epsilon=1e-6, **kwargs):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool = True,
+        device=None,
+        dtype=None,
+        epsilon=1e-6,
+        **kwargs
+    ):
         super().__init__(in_features, out_features, bias, device, dtype)
         self.epsilon = epsilon
 
     def forward(self, inputs):
         return lf.linear_epsilon(inputs, self.weight, self.bias, self.epsilon)
-    
+
 
 class RMSNormIdentity(nn.Module):
     def __init__(self, hidden_size, eps=1e-6):
@@ -43,16 +53,78 @@ class RMSNormIdentity(nn.Module):
 
     def forward(self, hidden_states):
         return lf.rms_norm_identity(hidden_states, self.weight, self.variance_epsilon)
-    
+
 
 class LayerNormEpsilon(nn.LayerNorm):
 
-    def __init__(self, normalized_shape, eps: float = 0.00001, elementwise_affine: bool = True, bias: bool = True, device=None, dtype=None):
+    def __init__(
+        self,
+        normalized_shape,
+        eps: float = 0.00001,
+        elementwise_affine: bool = True,
+        bias: bool = True,
+        device=None,
+        dtype=None,
+    ):
         super().__init__(normalized_shape, eps, elementwise_affine, bias, device, dtype)
 
     def forward(self, x):
         return lf.layer_norm(x, self.weight, self.bias, self.eps)
-    
+
+
+from typing import Any
+from peft.tuners.lora import Linear as LoraLinear
+
+
+class LoraLinearLRP(LoraLinear):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
+        self._check_forward_args(x, *args, **kwargs)
+        adapter_names = kwargs.pop("adapter_names", None)
+
+        if self.disable_adapters:
+            if self.merged:
+                self.unmerge()
+            result = self.base_layer(x, *args, **kwargs)
+        elif adapter_names is not None:
+            result = self._mixed_batch_forward(
+                x, *args, adapter_names=adapter_names, **kwargs
+            )
+        elif self.merged:
+            result = self.base_layer(x, *args, **kwargs)
+        else:
+            result = self.base_layer(x, *args, **kwargs)
+            torch_result_dtype = result.dtype
+            for active_adapter in self.active_adapters:
+                if active_adapter not in self.lora_A.keys():
+                    continue
+                lora_A = self.lora_A[active_adapter]
+                lora_B = self.lora_B[active_adapter]
+                dropout = self.lora_dropout[active_adapter]
+                scaling = self.scaling[active_adapter]
+                x = x.to(lora_A.weight.dtype)
+
+                if not self.use_dora[active_adapter]:
+                    # result = result + lora_B(lora_A(dropout(x))) * scaling
+                    result = lf.add2(
+                        result, lf.mul2(lora_B(lora_A(dropout(x))), scaling)
+                    )
+                else:
+                    x = dropout(x)
+                    # result = result + self._apply_dora(
+                    #     x, lora_A, lora_B, scaling, active_adapter
+                    # )
+                    result = lf.add2(
+                        result,
+                        self._apply_dora(x, lora_A, lora_B, scaling, active_adapter),
+                    )
+
+            result = result.to(torch_result_dtype)
+
+        return result
 
 ##################################
 ### MultiheadAttention Modules ###
@@ -67,10 +139,10 @@ class LinearInProjection(nn.Module):
 
         self.weight = weight
         self.bias = bias
-    
+
     def forward(self, x):
         return torch.nn.functional.linear(x, self.weight, self.bias)
-    
+
 class LinearOutProjection(nn.Module):
     """
     Custom nn.Linear module to make it easier to attach different rules to it.
@@ -80,15 +152,15 @@ class LinearOutProjection(nn.Module):
 
         self.weight = weight
         self.bias = bias
-    
+
     def forward(self, x):
         return torch.nn.functional.linear(x, self.weight, self.bias)
 
 class MultiheadAttention_CP(nn.Module):
     """
-    Implementing the CP-LRP (Conservative Propagation - LRP) rule for attention i.e. we don't let relevance flow through the softmax, but only through the value path. 
-    This method *only works well in Vision Transformers* because here the advanced AttnLRP rules, which do use the softmax, have similar performance to CP-LRP rules. 
-    The issue with AttnLRP is that using the softmax introduces gradient shattering, which requires applying the z-plus LRP rule. 
+    Implementing the CP-LRP (Conservative Propagation - LRP) rule for attention i.e. we don't let relevance flow through the softmax, but only through the value path.
+    This method *only works well in Vision Transformers* because here the advanced AttnLRP rules, which do use the softmax, have similar performance to CP-LRP rules.
+    The issue with AttnLRP is that using the softmax introduces gradient shattering, which requires applying the z-plus LRP rule.
     This makes AttnLRP slightly less efficient and, based on our limited experiments, the small performance gain is not worthwhile in Vision Transformers.
     However, in Large Language Models, applying AttnLRP on the softmax is substantially better than CP-LRP and does not require the less efficient z-plus rule.
     Therefore, we choose the more efficient CP-LRP for attention and use AttnLRP for other parts of the ViT.
@@ -116,13 +188,14 @@ class MultiheadAttention_CP(nn.Module):
 
         assert is_causal == False # not supported yet
 
-        return ls.multi_head_attention_cp(query, key, value, self.batch_first, self.num_heads, self.head_dim, self.q_proj_weight, self.bias_q, self.k_proj_weight, 
+        return ls.multi_head_attention_cp(query, key, value, self.batch_first, self.num_heads, self.head_dim, self.q_proj_weight, self.bias_q, self.k_proj_weight,
                                 self.bias_k, self.v_proj, self.out_proj, key_padding_mask, need_weights, attn_mask, average_attn_weights)
-    
-    
+
+
 ##########################
 ### Initialize Modules ###
 ##########################
+
 
 def copy_parameters_and_buffers_(original, replacement):
     """
@@ -140,7 +213,7 @@ def initialize_generic(original, replacement):
     """
     Initialize a replacement module with the correct arguments.
     """
-    
+
     kwargs = {}
     for arg in inspect.signature(original.__init__).parameters.keys():
         if hasattr(original, arg):
@@ -174,9 +247,9 @@ def initialize_MHA(original, replacement):
     """
     Initialize a MultiheadAttention_CP module.
     """
-    
+
     replacement = replacement()
-    
+
     if not original._qkv_same_embed_dim:
         replacement.q_proj_weight = original.q_proj_weight
         replacement.k_proj_weight = original.k_proj_weight
@@ -190,10 +263,10 @@ def initialize_MHA(original, replacement):
         replacement.bias_q = original.in_proj_bias[:original.embed_dim]
         replacement.bias_k = original.in_proj_bias[original.embed_dim:original.embed_dim*2]
         replacement.v_proj.bias = original.in_proj_bias[original.embed_dim*2:original.embed_dim*3]
-        
+
     if original.bias_k is not None:
         raise NotImplementedError("add_bias_kv=True is not supported yet.")
-    
+
     replacement.out_proj.weight = original.out_proj.weight
     replacement.out_proj.bias = original.out_proj.bias
 
@@ -204,6 +277,23 @@ def initialize_MHA(original, replacement):
 
     return replacement
 
+def initialize_adapter(original, replacement):
+    """
+    Initialize a replacement module with the correct arguments.
+    """
+    # kwargs = {}
+    # for arg in inspect.signature(original.__init__).parameters.keys():
+    #     if hasattr(original, arg):
+    #         kwargs[arg] = getattr(original, arg)
+
+    # kwargs["adapter_name"] = original._active_adapter[0]
+    # replacement = replacement(**kwargs)
+    # copy_parameters_and_buffers_(original, replacement)
+    import types
+
+    original.forward = types.MethodType(replacement.forward, original)
+    return original
+
 
 INIT_MODULE_MAPPING = {
     SoftmaxDT: initialize_generic,
@@ -211,4 +301,5 @@ INIT_MODULE_MAPPING = {
     RMSNormIdentity: initialize_generic,
     LayerNormEpsilon: initialize_bias,
     MultiheadAttention_CP: initialize_MHA,
+    LoraLinearLRP: initialize_adapter,
 }
